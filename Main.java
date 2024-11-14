557e13a2d9838abe9700615a78465592334feca0
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-11-14 11:03:21 -0800

    completed

--- Main.java
+++ Main.java
@@ -4,6 +4,19 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+
+    int userSum=0;
+    int userNum=0; 
+
+    while (userNum != -1)
+    {
+      userNum = sc.nextInt();
+      if (userNum != -1)
+      {
+        userSum += userNum;
+      }
+    }
+    System.out.println(userSum);
   }
 }
